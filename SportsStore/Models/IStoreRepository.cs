95205b616f6a4d8277bb6b813fdb51eaf564a093
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-04 12:49:53 +0700

    UPDATE - IStoreRepository.cs in SportsStore/Models - EFStoreRepository.cs - Product.cs - Products.razor

--- SportsStore/Models/IStoreRepository.cs
+++ SportsStore/Models/IStoreRepository.cs
@@ -3,6 +3,10 @@
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
-    }
 
+
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
+    }
 }
